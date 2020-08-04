50ffc88bd7e7f7b8d025e79b08a9e2f56338db7c
jamisoncarlen <52292802+jamisoncarlen@users.noreply.github.com>
2020-08-04 13:07:37 -0400

    Phase 2 added buttons for new topic and new mod
    I added buttons that load you a new topic and a new modifier seperately.  I used states and event handlers to do this.

--- src/Components/Generate.js
+++ src/Components/Generate.js
@@ -1,24 +1,52 @@
 import React from 'react';
+export default class Generate extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = { topic: "a gorilla", modifier: "with purple hair" };
+    }
+
+    handleClick(evt) {
+
+        const id = evt.target.getAttribute("id");
+        console.log("id: " + id)
+        //create topics array
+        let topics = require('../topics.json');
+
+        let tMax = topics.length;
+        var h = Math.floor(Math.random() * tMax);
+
+        console.log((h + 1) + " of " + topics.length);
+        console.log(topics[h]);
 
 
-export default function Generate(h, j) {
-    let topics = require('../topics.json');
-    let tMax = topics.length;
-    h = Math.floor(Math.random() * tMax);
-    console.log((h + 1) + " of " + topics.length);
-    console.log(topics[h]);
-  
-    //create modifiers array
-    let modifiers = require('../modifiers.json');
-    let mMax = modifiers.length;
-    j = Math.floor(Math.random() * mMax);
-    console.log((j + 1) + " of " + modifiers.length);
-    console.log(modifiers[j]);
+        //create modifiers array
+        let modifiers = require('../modifiers.json');
 
-    return(
-    
-        <div>
-            <h1>You must draw {topics[h]} {modifiers[j]}.</h1>
-        </div>
-    );
+        let mMax = modifiers.length;
+        var j = Math.floor(Math.random() * mMax);
+
+        console.log((j + 1) + " of " + modifiers.length);
+        console.log(modifiers[j]);
+
+        
+        if(id==="topic"){
+            //set topic state
+            this.setState({ topic: this.setState.topic = topics[h] });
+        } else {
+            //set modifier state
+            this.setState({ modifier: this.setState.modifier = modifiers[j] });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>You must draw {this.state.topic} {this.state.modifier}</h1>
+                <button id="topic" onClick={this.handleClick.bind(this.state.topic)}>New Topic</button>
+                <button id="modifier" onClick={this.handleClick.bind(this.state.modifier)}>New Modifier</button>
+            </div>
+        );
+    }
 }
